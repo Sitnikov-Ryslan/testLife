5bb1b167f1b71f08bad33231cff67b760a7b3ceb
Sitnikov-Ryslan <63663502+Sitnikov-Ryslan@users.noreply.github.com>
2020-08-16 17:00:07 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// Swiper
+
+let swipers = document.querySelectorAll('.swiper-container');
+
+swipers.forEach(function(elem) {
+    let mySwiper = new Swiper(elem, {
+        
+        speed: 400,
+        spaceBetween: 30,
+        slidesPerView: 3,
+        autoHeight: true,
+    
+        navigation: {
+            nextEl: elem.parentNode.querySelector('.swiper-button-next'),
+            prevEl: elem.parentNode.querySelector('.swiper-button-prev'),
+          },
+    })
+})
+
+// Fixed Header
+
 const header = document.querySelector('.header');
 const testsHeight =  document.querySelector('.tests').offsetTop;
 const nav = document.querySelector('.nav');
@@ -8,8 +29,6 @@ const modal = document.querySelector('.modal');
 const modalForm = document.querySelector('.modal__window');
 
 const likes = document.querySelectorAll('.test-preview__btn');
-
-// Fixed Header
 
 let fixedHeader = function() {
     let scrollOffset = window.pageYOffset;
@@ -56,21 +75,64 @@ likes.forEach(elem => {
     })
 })
 
-// Swiper
+// Toggle forms
 
-let swipers = document.querySelectorAll('.swiper-container');
+const accountToggleBtn = document.querySelector('.authorization__account');
+const loginToggleBtn = document.querySelector('.registration__login');
+const accForm = document.querySelector('.registration');
+const logForm = document.querySelector('.authorization');
+const modalTitle = document.querySelector('.modal__title');
 
-swipers.forEach(function(elem) {
-    let mySwiper = new Swiper(elem, {
-        
-        speed: 400,
-        spaceBetween: 30,
-        slidesPerView: 3,
-        autoHeight: true,
-    
-        navigation: {
-            nextEl: elem.parentNode.querySelector('.swiper-button-next'),
-            prevEl: elem.parentNode.querySelector('.swiper-button-prev'),
-          },
-    })
-})
+accountToggleBtn.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	accForm.classList.remove('hidden');
+    logForm.classList.add('hidden');
+    modalTitle.textContent = 'Регистрация';
+});
+
+loginToggleBtn.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	logForm.classList.remove('hidden');
+    accForm.classList.add('hidden');
+    modalTitle.textContent = 'Авторизация';
+});
+
+// Filled forms inputs
+
+const loginMailInput = document.querySelector('.authorization__input_email');
+const loginPassInput = document.querySelector('.authorization__input_pass');
+
+loginMailInput.addEventListener('change', function() {
+	if (loginMailInput.value !== '') {
+	loginMailInput.classList.add('filled');
+	} else {
+		loginMailInput.classList.remove('filled');
+	};
+});
+
+loginPassInput.addEventListener('change', function() {
+	if (loginPassInput.value !== '') {
+	loginPassInput.classList.add('filled');
+	} else {
+		loginPassInput.classList.remove('filled');
+	};
+});
+
+const accountMailInput = document.querySelector('.registration__input_email');
+const accountPassInput = document.querySelector('.registration__input_pass');
+
+accountMailInput.addEventListener('change', function() {
+	if (accountMailInput.value !== '') {
+	accountMailInput.classList.add('filled');
+	} else {
+		accountMailInput.classList.remove('filled');
+	};
+});
+
+accountPassInput.addEventListener('change', function() {
+	if (accountPassInput.value !== '') {
+	accountPassInput.classList.add('filled');
+	} else {
+		accountPassInput.classList.remove('filled');
+	};
+});
