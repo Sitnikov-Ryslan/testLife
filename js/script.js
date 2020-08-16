ebfba48120f79ae1db12bf83e8dc0de16f0b257b
Sitnikov-Ryslan <63663502+Sitnikov-Ryslan@users.noreply.github.com>
2020-08-16 20:23:49 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+// Fetch 
+
+const requestURL = '';
+
+function sendRequest(method, url, body = null) {
+    const headers = {
+        'Content-Type': "application/json"
+    }
+
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+    }).then(response => {
+        if (response.ok) {
+            return response.json()
+        }
+        return response.json().then(error => {
+            const e = new Error('Fatal error')
+            e.data = error
+            throw e
+        })
+    })
+}
+
+sendRequest('GET', requestURL)
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+
+// const body = {
+//     name: 'Ryslan',
+//     age: 28
+// }
+
+// sendRequest('POST', requestURL, body)
+//     .then(data => console.log(data))
+//     .catch(err => console.error(err))
+
 // Swiper
 
 let mySwiper = new Swiper('.swiper-container', {
